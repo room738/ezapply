3688c1871c08d7acd8809221f808dfd5c63d8aa9
Nikhi Bhambra <nikhibhambra@gmail.com>
2022-06-25 19:23:20 -0400

    Finished unfinished js scaffolding

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 let handleOnClick = () => {
-    console.log("_")
+    document.getElementById("more").style.display = "none"
+    document.getElementById("ff").style.marginRight = "0"
+    blurb = document.getElementById("blurb")
+    blurb.style.display = "block"
+    blurb.innerText = "Lorem Khaled Ipsum is a major key to success. Egg whites, turkey sausage, wheat toast, water. Of course they dont want us to eat our breakfast, so we are going to enjoy our breakfast. Cloth talk. Mogul talk. A major key, never panic. Dont panic, when it gets crazy and rough, dont panic, stay calm. You should never complain, complaining is a weak emotion, you got life, we breathing, we blessed. Learning is cool, but knowing is better, and I know the key to success. Egg whites, turkey sausage, wheat toast, water. Of course they dont want us to eat our breakfast, so we are going to enjoy our breakfast. Let me be clear, you have to make it through the jungle to make it to paradise, thats the key, Lion! Let me be clear, you have to make it through the jungle to make it to paradise, thats the key, Lion! I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. To be successful youve got to work hard, to make history, simple, youve got to make it. Cloth talk. Cloth talk. Every chance I get, I water the plants, Lion! Put it this way, it took me twenty five years to get these plants, twenty five years of blood sweat and tears"
+    document.getElementById("main-text").innerText = "basically, job apps suck"
 }
